aa9a3f6cdc125d9a2b1aade93a9fb6dd1111fe3e
enxa <enxa@mail.com>
2021-12-10 13:40:22 +0100

    add photos gallery

--- src/views/Photos.vue
+++ src/views/Photos.vue
@@ -1,19 +1,43 @@
 <script setup>
-  import { defineProps, toRefs, onMounted } from 'vue'
+  import { ref, defineProps, toRefs, onMounted } from 'vue'
 
+  const clientId = 'uYB88IY7UH3wsQFu9t3lWf5jM01GiTA8l441NSk0r8k'
   const props = defineProps({
     searchPhrase: String
   })
 
-  const { searchPhrase } = toRefs(props)
+  let { searchPhrase } = toRefs(props)
+  let photos = ref([])
 
-  onMounted(() => {
-    
+  onMounted(async () => {
+    const data = await fetch(`https://api.unsplash.com/search/photos?query=${searchPhrase.value}&client_id=${clientId}`)
+    const json = await data.json()
+
+    json.results.forEach(result => {
+      photos.value.push(result.urls.small)
+    })
   })
 </script>
 
 <template>
-  <div>
-    {{searchPhrase}}
+  <div class="gallery">
+    <img v-for="(photo, i) in photos" :src="photo" :alt="i">
   </div>
 </template>
+
+<style>
+  .gallery {
+    display: flex;
+    flex-flow: column wrap;
+    max-height: 150vh;
+    max-width: 1260px;
+    margin: auto;
+    overflow: hidden;
+  }
+    .gallery img {
+      width: 100%;
+      max-width: 400px;
+      margin: 10px;
+    }
+    
+</style>
